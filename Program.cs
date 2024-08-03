551f8f5ef32813f905e88de324f00c1e90f30773
IVadim23 <inazrski@gmail.com>
2024-08-04 00:32:52 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _Deposit_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Add deposit: ");
+            double deposit = double.Parse(Console.ReadLine());
+            Console.WriteLine("Add term: ");
+            int term = int.Parse(Console.ReadLine());
+            Console.WriteLine("Add rate: " );
+            double rate = double.Parse(Console.ReadLine());
+            double sum = deposit + term * ((deposit * rate / 100) / 12);
+            Console.WriteLine("The final sum is equal to: \n {0:f2}",sum);
+        }
+    }
+}
